12751b7204100a3945a3c00d110e7c4130d62b85
Filipponik <mitin.maks@bk.ru>
2023-01-07 19:51:58 +0300

    Added short syntax for lang files

--- src/Formatter.php
+++ src/Formatter.php
@@ -6,17 +6,41 @@ namespace Filipponik\TranslateAnalyzer;
 
 class Formatter
 {
+    public static int $spacesPerTabCount = 4;
+
     // TODO Save to Laravel default directory structure
     public static function createTransactionFiles(string $directoryName, array $keys, array $unsupportedKeys): void
     {
         foreach ($keys as $filename => $key) {
-            $info = "<?php\n\nreturn " . var_export($key, true) . ";\n";
+            $customVarExport = self::customVarExport($key);
+
+            $info = "<?php\n\nreturn " . $customVarExport . ";\n";
             Helper::saveToFile($directoryName, $filename . '.php', $info);
         }
 
         if (!empty($unsupportedKeys)) {
             $info = implode("\n", $unsupportedKeys);
             Helper::saveToFile($directoryName, 'UNSUPPORTED', $info);
+        }
+    }
+
+    public static function customVarExport($inputValue, int $spacesCount = 0): string
+    {
+        if (is_array($inputValue)) {
+            $spaces = str_repeat(' ', $spacesCount);
+            $summary = "[\n";
+            $innerSpacesCount = $spacesCount + self::$spacesPerTabCount;
+            $innerSpaces = str_repeat(' ', $innerSpacesCount);
+
+            foreach ($inputValue as $key => $value) {
+                $exportedVar  = self::customVarExport($value, $innerSpacesCount);
+                $summary .= "$innerSpaces'$key' => $exportedVar\n";
+            }
+            $summary .= $spaces . ']';
+
+            return $summary;
+        } else {
+            return "'$inputValue'";
         }
     }
 }
